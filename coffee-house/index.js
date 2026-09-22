3edc1b28fdec8368f10d1c269359053af2c20e99
kotovnik <kotovnik2024tbilisi@gmail.com>
2026-09-22 18:07:21 +0400

    feat: constants to js file

--- coffee-house/index.js
+++ coffee-house/index.js
@@ -1,17 +1,26 @@
 // DARK__MODE
 
 // <button class="mode__wrapper mode__wrapper_light" id="mode-toggle">
+const modeToggle = document.getElementById('mode-toggle');
+console.log(modeToggle);
+const modeWrapper = modeToggle;
 // убрать .mode__wrapper_light
 // добавить .mode__wrapper_dark
 
-// <div class="sun__box moon__box_light">
-// убрать .moon__box_light
+// <div class="sun__box sun__box_light">
+const sunBox = modeToggle.querySelector('.sun__box');
+// console.log(sunBox);
+// убрать .sun__box_light
 // добавить .sun__box_dark
 
 // <div class="moon__box moon__box_light">
+const moonBox = modeToggle.querySelector('.moon__box');
+// console.log(moonBox);
 // убрать moon__box_light
 // добавить moon__box_dark
 
 // <svg class="moon-icon moon-icon_light"
+const moonIcon = modeToggle.querySelector('.moon-icon');
+console.log(moonIcon);
 // убрать класс moon-icon_light
 // добавить moon-icon_dark
