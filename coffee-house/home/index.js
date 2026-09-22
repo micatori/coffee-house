d55a759f9ff097a389cd7e06b5088546da409721
kotovnik <kotovnik2024tbilisi@gmail.com>
2026-09-22 15:19:23 +0400

    feat: add dark mode styles for both pages

---
+++ coffee-house/home/index.js
@@ -0,0 +1,17 @@
+// DARK__MODE
+
+// <button class="mode__wrapper mode__wrapper_light" id="mode-toggle">
+// убрать .mode__wrapper_light
+// добавить .mode__wrapper_dark
+
+// <div class="sun__box moon__box_light">
+// убрать .moon__box_light
+// добавить .sun__box_dark
+
+// <div class="moon__box moon__box_light">
+// убрать moon__box_light
+// добавить moon__box_dark
+
+// <svg class="moon-icon moon-icon_light"
+// убрать класс moon-icon_light
+// добавить moon-icon_dark
